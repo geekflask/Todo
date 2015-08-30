11afb0927767095e3a983361f6869564cbe3d09e
geekflask <geekflask@gmail.com>
2015-08-30 14:40:21 +0800

    init

---
+++ tests/test_todo.py
@@ -0,0 +1,22 @@
+import unittest
+from app import app
+from app.models import Todo
+
+
+class TodoTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def tearDown(self):
+        todos = Todo.objects.all()
+        for todo in todos:
+            todo.delete()
+
+    def test_index(self):
+        rv = self.app.get('/')
+        assert "Todo" in rv.data
+
+    def test_todo(self):
+        self.app.post('/add', data = dict(content="testtodo"))
+        todo = Todo.objects.get_or_404(content="testtodo")
+        assert todo is not None
